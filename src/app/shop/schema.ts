b70bcd1de806a947357799e1cacacdda03898f2a
Sergio Nizama <s.nizama@gmail.com>
2024-03-11 02:23:41 +0900

    - Added some shadcn/ui components for comic/products. - Cleaned up main layout code. - Redirecting to shop from home page. - Added EjunkieProduct schema type.

---
+++ src/app/shop/schema.ts
@@ -0,0 +1,15 @@
+export type EjunkieProduct = {
+  id: string
+  name: string
+  price: string
+  currency: string
+  description: string
+  details: string
+  image: string
+  thumbnail: string
+  tagline: string
+  purchased: number
+  tags: (null | string)[]
+  file_size: string
+  images: (null | string)[]
+}
