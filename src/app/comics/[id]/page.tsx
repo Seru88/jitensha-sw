317e0f8bf2eeb623120c68baaa4e657c2496f447
Sergio Nizama <s.nizama@gmail.com>
2024-03-20 17:09:31 +0900

    added eslint for prettier

--- src/app/comics/[id]/page.tsx
+++ src/app/comics/[id]/page.tsx
@@ -1,24 +1,26 @@
-import { getProduct, getProducts } from '@/lib/ejunkie-api'
-import { EjunkieProduct } from '../schema'
+import { getProduct, getProducts } from "@/lib/ejunkie-api";
+import { EjunkieProduct } from "../schema";
 
 type Props = {
-  params: { id: string }
-}
+  params: { id: string };
+};
 
 export async function generateStaticParams() {
-  const data = await getProducts()
-  return (data.products as EjunkieProduct[]).map(comic => ({
+  const data = await getProducts();
+  return (data.products as EjunkieProduct[]).map((comic) => ({
     id: comic.id,
-  }))
+  }));
 }
 
 export default async function ComicProductPage({ params }: Props) {
-  const data = await getProduct(params.id)
-  const products = data.products as EjunkieProduct[]
-  const comic = products.length ? products[0] : null
+  const data = await getProduct(params.id);
+  const products = data.products as EjunkieProduct[];
+  const comic = products.length ? products[0] : null;
 
-  return <div>
-    <h2>{comic?.name}</h2>
-    <span>{comic?.price}</span>
-  </div>
+  return (
+    <div>
+      <h2>{comic?.name}</h2>
+      <span>{comic?.price}</span>
+    </div>
+  );
 }
